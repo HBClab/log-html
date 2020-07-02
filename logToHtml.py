d143ee4f1376b4a5ff71ef0a0fc7f3a07d27477d
zkhan12 <zoheb1112@gmail.com>
2020-07-02 17:45:30 -0500

    add main func; add default template path

--- logToHtml.py
+++ logToHtml.py
@@ -1,6 +1,7 @@
 import os
 import jinja2
 from argparse import ArgumentParser, RawTextHelpFormatter
+from glob import glob
 
 
 def build_parser():
@@ -8,48 +9,61 @@ def build_parser():
                     description='accelBIDSTransform BIDS args',
                     formatter_class=RawTextHelpFormatter
                     )
-    parser.add_argument('log_dir', help='directory where log '
+    parser.add_argument('log_dir', help='(abs path) directory where log '
                                         'files are mounted')
     parser.add_argument('output_file', help='location of the html output')
-    parser.add_argument('template_file', help='location of the html template')
+    parser.add_argument('--template-file', help='location of the html template',
+                        required=False, default='templates/temp.tpl')
 
     return parser
 
 
-class Accel():
-    def __init__(self, filename, status, path, date, time):
-        self.filename = filename
-        self.status = status
-        if status == 'ERROR':
-            self.css = 'redtext'
+def main():
+    class Accel():
+        def __init__(self, filename, status, path, date, time):
+            self.filename = filename
+            self.status = status
+            if status == 'ERROR':
+                self.css = 'redtext'
+            else:
+                self.css = 'greentext'
+            self.path = path
+            self.date = date
+            self.time = time
+
+    opts = build_parser().parse_args()
+    # converting relative paths to absolute
+    template_file = os.path.abspath(opts.template_file)
+    log_dir = os.path.abspath(opts.log_dir)
+    os.chdir(os.path.dirname(log_dir))
+    log_dir_base = os.path.basename(log_dir)
+
+    # glob returns whatever path type you pass into it
+    # in this case it returns relative paths
+    filenames = glob(os.path.join(log_dir_base, '*'))
+
+    accel_files = []
+
+    for fil in filenames:
+        contents = open(fil, 'r').read()
+        date = contents[0:10]
+        time = contents[11:19]
+        if 'ERROR' in contents:
+            accel_files.append(Accel(os.path.basename(fil), 'ERROR', 'file:' +
+                                     fil, date, time))
         else:
-            self.css = 'greentext'
-        self.path = path
-        self.date = date
-        self.time = time
+            accel_files.append(Accel(os.path.basename(fil), 'SUCCESS', 'file:' +
+                                     fil, date, time))
+
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(searchpath=os.path.dirname(template_file))
+    )
+    report_tpl = env.get_template(os.path.basename(template_file))
+    report_render = report_tpl.render(accel_files=accel_files)
+
+    with open(os.path.join(os.path.dirname(log_dir), opts.output_file), 'w+') as file:
+        file.write(report_render)
 
 
-opts = build_parser().parse_args()
-(_, _, filenames) = next(os.walk(opts.log_dir))
-
-accel_files = []
-
-for fil in filenames:
-    contents = open(opts.log_dir + fil, 'r').read()
-    date = contents[0:10]
-    time = contents[11:19]
-    if 'ERROR' in contents:
-        accel_files.append(Accel(fil, 'ERROR', 'file:' +
-                                 os.path.join(opts.log_dir, fil), date, time))
-    else:
-        accel_files.append(Accel(fil, 'SUCCESS', 'file:' +
-                                 os.path.join(opts.log_dir, fil), date, time))
-
-env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(searchpath=os.path.dirname(opts.template_file))
-)
-report_tpl = env.get_template(os.path.basename(opts.template_file))
-report_render = report_tpl.render(accel_files=accel_files)
-
-with open(opts.output_file, 'w+') as file:
-    file.write(report_render)
+if __name__ == "__main__":
+    main()
